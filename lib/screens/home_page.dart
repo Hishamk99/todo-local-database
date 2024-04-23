2ad1680f696e2147741ed0477e80b68ce04298cc
hishamkame <hishamkamel370@gmail.com>
2024-04-23 21:52:04 +0200

    change theme

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.yellow.shade200,
+        appBar: AppBar(),
+      ),
+    );
+  }
+}
